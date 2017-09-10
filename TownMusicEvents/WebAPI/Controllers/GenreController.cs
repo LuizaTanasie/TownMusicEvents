897f485b2b9c7943c1852c43f995beac2f3cbd83
luizatanasie <luiza.tanasie@gmail.com>
2017-09-10 13:07:12 +0300

    api start

---
+++ TownMusicEvents/WebAPI/Controllers/GenreController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebAPI.Services
+{
+    public class GenreController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var service = new GenreService();
+            var genres = service.GetAllCategories();
+            if (genres.Count == 0)
+                return NotFound();
+            return Ok(genres);
+        }
+    }
+}
---
+++ TownMusicEvents/WebAPI/Services/GenreService.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TownMusicEvents;
+using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public class GenreService
+    {
+        public List<GenreModel> GetAllCategories()
+        {
+            var genreModels = new List<GenreModel>();
+            using (var unitOfWork = new UnitOfWork())
+            {
+                var genreRepository = unitOfWork.GetRepository<Genre>();
+                foreach (var genre in genreRepository.GetAll())
+                {
+                    genreModels.Add(new GenreModel { Id = genre.Id, Name = genre.Name });
+                }
+                return genreModels;
+            }
+        }
+    }
+}
